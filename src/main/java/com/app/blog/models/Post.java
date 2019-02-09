2510c870556898bf00dc3c6a6887093841e77f72
dejanilic <ilicdejan1997@gmail.com>
2019-02-09 14:03:30 +0100

    Add Post class

---
+++ src/main/java/com/app/blog/models/Post.java
@@ -0,0 +1,35 @@
+package com.app.blog.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+    @Lob
+    private String content;
+    private String dateCreated;
+    private String createdBy;
+    private String dateModified;
+    private String modifiedBy;
+
+    // user id
+
+    // blog id
+    @ManyToMany
+    @JoinTable(name = "post_blog", joinColumns = @JoinColumn(name = "post_id"),
+    inverseJoinColumns = @JoinColumn(name = "blog_id"))
+    private Set<Blog> blogs = new HashSet<Blog>();
+}
