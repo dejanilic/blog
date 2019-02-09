f344af14388d711266fba1fff3f6578bbbf5d300
dejanilic <ilicdejan1997@gmail.com>
2019-02-09 14:20:22 +0100

    Add Role repositorium

---
+++ src/main/java/com/app/blog/repositories/RoleRepositorium.java
@@ -0,0 +1,7 @@
+package com.app.blog.repositories;
+
+import com.app.blog.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepositorium extends CrudRepository<Role, Long> {
+}
