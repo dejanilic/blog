e112cb2f0697bd9fa1931f0ed223069cd6141afd
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 17:33:34 +0100

    Add getRoleByPosition method

--- src/main/java/com/app/blog/repositories/RoleRepositorium.java
+++ src/main/java/com/app/blog/repositories/RoleRepositorium.java
@@ -1,7 +1,11 @@
 package com.app.blog.repositories;
 
+import com.app.blog.models.Position;
 import com.app.blog.models.Role;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RoleRepositorium extends CrudRepository<Role, Long> {
+    Optional<Role> getRoleByPosition(Position position);
 }
