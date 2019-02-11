fa6e216219f6d5965c53eab3e70f2f8906234ffe
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 17:31:03 +0100

    Add UserController class

--- src/main/java/com/app/blog/controllers/UserController.java
+++ src/main/java/com/app/blog/controllers/UserController.java
@@ -1,8 +1,16 @@
 package com.app.blog.controllers;
 
+import com.app.blog.commands.UserCommand;
 import com.app.blog.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -13,5 +21,18 @@ public class UserController {
         this.userService = userService;
     }
 
+    @RequestMapping(value = {"", "/", "/login"}, method = RequestMethod.GET)
+    public String showLoginPage(Model model) {
+        model.addAttribute("user", new UserCommand());
+        return "login";
+    }
 
+    @RequestMapping(value = "user", method = RequestMethod.POST)
+    public void login(@Valid @ModelAttribute("user") UserCommand command, BindingResult result) {
+        // provera za formu
+
+        // logika za prikaz stranica
+        String menu = userService.validate(command);
+
+    }
 }
