cf9672682611543364f104fc18e503b04c9b5eff
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 13:47:02 +0100

    Add EqualsAndHashCode annotation

--- src/main/java/com/app/blog/models/Post.java
+++ src/main/java/com/app/blog/models/Post.java
@@ -1,5 +1,6 @@
 package com.app.blog.models;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.Set;
 
 
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = {"user", "blog"})
 @Getter
 @Setter
 @Entity
