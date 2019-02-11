866a9fe463694e270356a74eeb6d893bdfa2c890
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 13:47:24 +0100

    Add UserCommand class

---
+++ src/main/java/com/app/blog/commands/UserCommand.java
@@ -0,0 +1,14 @@
+package com.app.blog.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserCommand {
+    private Long id;
+    private String username;
+    private String password;
+}
