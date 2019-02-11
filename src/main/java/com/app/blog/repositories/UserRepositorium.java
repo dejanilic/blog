b5e6c20f1de9e46793993ccb1020ae2d3a0afd6e
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 17:34:04 +0100

    Add findByUsernameAndPassword method

--- src/main/java/com/app/blog/repositories/UserRepositorium.java
+++ src/main/java/com/app/blog/repositories/UserRepositorium.java
@@ -3,5 +3,8 @@ package com.app.blog.repositories;
 import com.app.blog.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepositorium extends CrudRepository<User, Long> {
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
