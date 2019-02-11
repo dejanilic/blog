4b214148701622b22313a99133d490ef0b2f293a
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 17:35:03 +0100

    Add validation methods

--- src/main/java/com/app/blog/services/IUser.java
+++ src/main/java/com/app/blog/services/IUser.java
@@ -1,4 +1,12 @@
 package com.app.blog.services;
 
+import com.app.blog.commands.UserCommand;
+import com.app.blog.models.User;
+
+import java.util.Set;
+
 public interface IUser {
+    Set<User> getUsers();
+    Boolean exists(User user);
+    String validate(UserCommand userCommand);
 }
