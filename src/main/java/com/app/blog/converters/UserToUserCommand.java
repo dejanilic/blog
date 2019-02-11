45e4d2d999992ba091db6ed3c7230e8ae5d433aa
dejanilic <ilicdejan1997@gmail.com>
2019-02-11 17:32:43 +0100

    Add UserToUserCommand class

--- src/main/java/com/app/blog/converters/UserToUserCommand.java
+++ src/main/java/com/app/blog/converters/UserToUserCommand.java
@@ -5,7 +5,9 @@ import com.app.blog.models.User;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserToUserCommand implements Converter<User, UserCommand> {
 
     @Synchronized
