5897c115e1692d58eefae3b70c87b414bbcc66d5
dejanilic <ilicdejan1997@gmail.com>
2019-02-13 15:52:15 +0100

    Add UserController class

--- src/main/java/com/app/blog/controllers/UserController.java
+++ src/main/java/com/app/blog/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.app.blog.controllers;
 
 import com.app.blog.commands.UserCommand;
+import com.app.blog.services.RoleService;
 import com.app.blog.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -16,15 +17,31 @@ import javax.validation.Valid;
 @Controller
 public class UserController {
     private final UserService userService;
+    private final RoleService roleService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
     @RequestMapping(value = {"", "/", "/login"}, method = RequestMethod.GET)
     public String showLoginPage(Model model) {
         model.addAttribute("user", new UserCommand());
         return "login";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegisterPage(Model model) {
+        model.addAttribute("user", new UserCommand());
+        model.addAttribute("role", roleService.getAllRoles());
+        return "register";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String saveUser(@Valid @ModelAttribute("login") UserCommand command, BindingResult result) {
+        // check binding result
+        userService.saveUser(command);
+        return "redirect:/login";
     }
 
     @RequestMapping(value = "user", method = RequestMethod.POST)
