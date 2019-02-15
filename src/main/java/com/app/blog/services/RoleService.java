fc328d6ce6b60bb797aace76c45cbf66c418e66a
dejanilic <ilicdejan1997@gmail.com>
2019-02-15 16:29:31 +0100

    Add RoleService class

--- src/main/java/com/app/blog/services/RoleService.java
+++ src/main/java/com/app/blog/services/RoleService.java
@@ -3,11 +3,13 @@ package com.app.blog.services;
 import com.app.blog.converters.RoleToRoleCommand;
 import com.app.blog.models.Role;
 import com.app.blog.repositories.RoleRepositorium;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RoleService implements IRole {
     private final RoleRepositorium roleRepositorium;
@@ -19,7 +21,8 @@ public class RoleService implements IRole {
     }
 
     @Override
-    public Set<Role> getAllRoles() {
+    public Set<Role> getRoles() {
+        log.info("getting all roles");
         Iterable<Role> source = roleRepositorium.findAll();
         Set<Role> roles = new HashSet<>();
         source.forEach(r -> roleToRoleCommand.convert(r));
