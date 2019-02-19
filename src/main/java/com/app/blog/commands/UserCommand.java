995d5b3963f3b9ac47ccdec53c730b9626987fe4
dejanilic <ilicdejan1997@gmail.com>
2019-02-19 14:38:06 +0100

    Add UserCommand class

--- src/main/java/com/app/blog/commands/UserCommand.java
+++ src/main/java/com/app/blog/commands/UserCommand.java
@@ -1,16 +1,30 @@
 package com.app.blog.commands;
 
+import com.app.blog.models.Position;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 1, max = 20)
     private String username;
+
+    @NotBlank
+    @Size(min = 1)
     private String password;
+
+    @NotBlank
+    @Email
     private String email;
-    private RoleCommand role;
+    private Position position;
 }
