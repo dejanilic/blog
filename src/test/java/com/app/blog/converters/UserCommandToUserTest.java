d35e26d40ab72c66f526e358e2463d1befc94653
dejanilic <ilicdejan1997@gmail.com>
2019-02-19 14:33:31 +0100

    Test UserCommandToUserTest class

--- src/test/java/com/app/blog/converters/UserCommandToUserTest.java
+++ src/test/java/com/app/blog/converters/UserCommandToUserTest.java
@@ -1,6 +1,7 @@
 package com.app.blog.converters;
 
 import com.app.blog.commands.UserCommand;
+import com.app.blog.models.Position;
 import com.app.blog.models.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,12 +12,14 @@ public class UserCommandToUserTest {
     private static final Long ID_VALUE = new Long(1L);
     private static final String USERNAME = "admin";
     private static  final String PASSWORD = "admin";
+    private static final String EMAIL = "mail@mail.com";
+    private static final Position POSITION = Position.ADMINISTRATOR;
 
     private UserCommandToUser converter;
 
     @Before
     public void setUp() throws Exception {
-        converter = new UserCommandToUser(new RoleCommandToRole());
+        converter = new UserCommandToUser();
     }
 
     @Test
@@ -36,6 +39,8 @@ public class UserCommandToUserTest {
         userCommand.setId(ID_VALUE);
         userCommand.setUsername(USERNAME);
         userCommand.setPassword(PASSWORD);
+        userCommand.setEmail(EMAIL);
+        userCommand.setPosition(POSITION);
 
         // when
         User user = converter.convert(userCommand);
@@ -44,5 +49,7 @@ public class UserCommandToUserTest {
         assertEquals(ID_VALUE, user.getId());
         assertEquals(USERNAME, user.getUsername());
         assertEquals(PASSWORD, user.getPassword());
+        assertEquals(EMAIL, user.getEmail());
+        assertEquals(POSITION, user.getPosition());
     }
 }
