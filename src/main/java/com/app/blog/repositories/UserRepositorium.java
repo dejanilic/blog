fa047fb9b638bca6367a06aa139d5ec722a4a6e8
dejanilic <ilicdejan1997@gmail.com>
2019-02-19 14:40:29 +0100

    Add UserRepositorium class

--- src/main/java/com/app/blog/repositories/UserRepositorium.java
+++ src/main/java/com/app/blog/repositories/UserRepositorium.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepositorium extends CrudRepository<User, Long> {
     Optional<User> findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsername(String username);
 }
