4e9a57c2303f821f57297d52aba1eedb5b6fac7f
dejanilic <ilicdejan1997@gmail.com>
2019-02-22 14:56:44 +0100

    Add PostCommandToPost class

---
+++ src/main/java/com/app/blog/converters/PostCommandToPost.java
@@ -0,0 +1,28 @@
+package com.app.blog.converters;
+
+import com.app.blog.commands.PostCommand;
+import com.app.blog.models.Post;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostCommandToPost implements Converter<PostCommand, Post> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Post convert(PostCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Post post = new Post();
+        post.setId(source.getId());
+        post.setTitle(source.getTitle());
+        post.setContent(source.getContent());
+
+        return post;
+    }
+}
