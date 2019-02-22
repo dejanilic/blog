000910a2172a3b79470ba078e2ffae8eab927173
dejanilic <ilicdejan1997@gmail.com>
2019-02-22 14:56:16 +0100

    Add PostCommand class

---
+++ src/main/java/com/app/blog/commands/PostCommand.java
@@ -0,0 +1,20 @@
+package com.app.blog.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostCommand {
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+}
