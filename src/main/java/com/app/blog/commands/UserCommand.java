56810ed4cfa2018bed65100ad734b7866fb4d0a0
dejanilic <ilicdejan1997@gmail.com>
2019-02-26 17:37:25 +0100

    Add UserCommand class

--- src/main/java/com/app/blog/commands/UserCommand.java
+++ src/main/java/com/app/blog/commands/UserCommand.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -26,5 +27,7 @@ public class UserCommand {
     @NotBlank
     @Email
     private String email;
+
+    @NotNull
     private Position position;
 }
