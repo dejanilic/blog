0ce75d78bd8898561d3fcb60b7d10036289c42db
dejanilic <ilicdejan1997@gmail.com>
2019-02-26 17:38:38 +0100

    Add PostRepositorium class

--- src/main/java/com/app/blog/repositories/PostRepositorium.java
+++ src/main/java/com/app/blog/repositories/PostRepositorium.java
@@ -3,5 +3,9 @@ package com.app.blog.repositories;
 import com.app.blog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PostRepositorium extends CrudRepository<Post, Long> {
+    Optional<Post> findByBlogId(Long id);
+    Iterable<Post> findAllByBlogId(Long id);
 }
