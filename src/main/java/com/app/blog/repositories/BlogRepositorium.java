2c641165b7d307fb508d88dc5ed0d232e97a74e6
dejanilic <ilicdejan1997@gmail.com>
2019-02-26 17:38:20 +0100

    Add BlogRepositorium class

--- src/main/java/com/app/blog/repositories/BlogRepositorium.java
+++ src/main/java/com/app/blog/repositories/BlogRepositorium.java
@@ -3,5 +3,8 @@ package com.app.blog.repositories;
 import com.app.blog.models.Blog;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BlogRepositorium extends CrudRepository<Blog, Long> {
+    Optional<Blog> findByTitle(String title);
 }
